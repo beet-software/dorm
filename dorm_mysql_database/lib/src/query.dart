98f499e382bab92c015d0393ff6a3726a76e038a
Enzo Santos <enzo.santos29@hotmail.com>
2024-06-16 12:38:27 -0300

    [dorm_mysql_database] feat: implements `Filter.date`

--- dorm_mysql_database/lib/src/query.dart
+++ dorm_mysql_database/lib/src/query.dart
@@ -1,3 +1,4 @@
+import 'package:dartx/dartx.dart';
 import 'package:dorm_framework/dorm_framework.dart';
 
 enum _QueryType { limit, sorted }
@@ -29,8 +30,90 @@ class Query implements BaseQuery<Query> {
 
   @override
   Query whereDate(String key, DateTime date, DateFilterUnit unit) {
-    // TODO: implement whereDate
-    throw UnimplementedError();
+    String format(DateTime date) {
+      return (StringBuffer()
+            ..write('${date.year}'.padLeft(4, '0'))
+            ..write('-')
+            ..write('${date.month}'.padLeft(2, '0'))
+            ..write('-')
+            ..write('${date.day}'.padLeft(2, '0'))
+            ..write(' ')
+            ..write('${date.hour}'.padLeft(2, '0'))
+            ..write(':')
+            ..write('${date.minute}'.padLeft(2, '0'))
+            ..write(':')
+            ..write('${date.second}'.padLeft(2, '0')))
+          .toString();
+    }
+
+    DateTime firstTimeOfDay(DateTime date) {
+      return date.date;
+    }
+
+    DateTime lastTimeOfDay(DateTime date) {
+      return date.date
+          .add(const Duration(days: 1))
+          .subtract(const Duration(microseconds: 1));
+    }
+
+    final DateTime startDate;
+    final DateTime endDate;
+    switch (unit) {
+      case DateFilterUnit.year:
+        startDate = date.firstDayOfYear;
+        endDate = lastTimeOfDay(date.lastDayOfYear);
+      case DateFilterUnit.month:
+        startDate = date.firstDayOfMonth;
+        endDate = lastTimeOfDay(date.lastDayOfMonth);
+      case DateFilterUnit.day:
+        startDate = firstTimeOfDay(date);
+        endDate = lastTimeOfDay(date);
+      case DateFilterUnit.hour:
+        startDate = firstTimeOfDay(date).copyWith(
+          hour: date.hour,
+        );
+        endDate = lastTimeOfDay(date).copyWith(
+          hour: date.hour,
+        );
+      case DateFilterUnit.minute:
+        startDate = firstTimeOfDay(date).copyWith(
+          hour: date.hour,
+          minute: date.minute,
+        );
+        endDate = lastTimeOfDay(date).copyWith(
+          hour: date.hour,
+          minute: date.minute,
+        );
+      case DateFilterUnit.second:
+        startDate = firstTimeOfDay(date).copyWith(
+          hour: date.hour,
+          minute: date.minute,
+          second: date.second,
+        );
+        endDate = lastTimeOfDay(date).copyWith(
+          hour: date.hour,
+          minute: date.minute,
+          second: date.second,
+        );
+      case DateFilterUnit.milliseconds:
+        startDate = firstTimeOfDay(date).copyWith(
+          hour: date.hour,
+          minute: date.minute,
+          second: date.second,
+          millisecond: date.millisecond,
+        );
+        endDate = lastTimeOfDay(date).copyWith(
+          hour: date.hour,
+          minute: date.minute,
+          second: date.second,
+          millisecond: date.millisecond,
+        );
+    }
+    // https://stackoverflow.com/a/14104364/9997212
+    return Query(
+      '$query WHERE $key BETWEEN \'${format(startDate)}\' AND \'${format(endDate)}\'',
+      params: {...params},
+    );
   }
 
   @override
