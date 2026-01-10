74dc2ee7f224f9adf554a8baaf81856ae50c7ed6
Enzo Santos <enzo.santos29@hotmail.com>
2026-01-10 08:39:09 -0300

    [dorm_generator] feat: adds const constructor to OrmGenerator

--- dorm_generator/lib/src/generator.dart
+++ dorm_generator/lib/src/generator.dart
@@ -1265,6 +1265,8 @@ extension _BaseWriting on Map<String, FieldOrmNode> {
 
 /// Code generator.
 class OrmGenerator extends Generator {
+  const OrmGenerator();
+
   @override
   String? generate(LibraryReader library, BuildStep buildStep) {
     final Set<Uri> partUris = library.element.parts
