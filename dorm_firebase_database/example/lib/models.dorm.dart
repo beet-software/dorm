9d04df63725e73b5eff880f696a8cbd8c541bea9
Enzo Santos <enzo.santos29@hotmail.com>
2026-09-05 13:40:02 -0300

    [dorm_firebase_database] feat: update example to accept I as type parameter

--- dorm_firebase_database/example/lib/models.dorm.dart
+++ dorm_firebase_database/example/lib/models.dorm.dart
@@ -1,4 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// dart format width=80
 
 part of 'models.dart';
 
@@ -61,7 +62,7 @@ class UserDependency extends Dependency<UserData> {
   const UserDependency() : super.strong();
 }
 
-class UserEntity implements Entity<UserData, User> {
+class UserEntity implements Entity<UserData, User, String> {
   const UserEntity();
 
   @override
@@ -117,7 +118,7 @@ class Dorm {
 
   final BaseEngine _engine;
 
-  DatabaseEntity<UserData, User> get users => DatabaseEntity(
+  DatabaseEntity<UserData, User, String> get users => DatabaseEntity(
         const UserEntity(),
         engine: _engine,
       );
